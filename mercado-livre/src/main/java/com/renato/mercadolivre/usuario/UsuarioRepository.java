49fee441d8b4dd7ec43ca746d9bcf0dcf33998aa
Jrenatomt <jrenatomt@gmail.com>
2021-03-26 11:35:52 -0300

    email duplicado restrição

--- mercado-livre/src/main/java/com/renato/mercadolivre/usuario/UsuarioRepository.java
+++ mercado-livre/src/main/java/com/renato/mercadolivre/usuario/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package com.renato.mercadolivre.usuario;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+	
+	Optional<Usuario> findByLogin(String login);
 
 }
